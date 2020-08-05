8f8929b1125bae1e50dd1953a99e5803224e2b26
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-05 10:44:59 -0700

    updated script and index

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // <!-- **START NOTE** I was working on this in a group with others. I have changed variables and tried to play around with the concept and functions. **END NOTE**  -->
-        
+$(document).ready(function(){
+  
+
+  function makeHistory(text) {
+    var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+    $(".history").append(li)
+  }
         
         // this is was added to give BUTTON line 17 --> a clickable event by .getElementById() & then .addEventListener to make that button a clickable button. //
         document.getElementById('search').addEventListener('click', event => {
@@ -13,19 +19,33 @@
             // **This is just the day's URL weather** //
             axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=606e8adf3bbd843811975dde31618837`)            
                 .then(res => {
+
+
+                    if (history.indexOf(city) === -1) {
+                      history.push(city);
+                      localStorage.setItem("histor", JSON.stringify(history));
+
+                      makeHistory(city);
+                    }
+
+
+
                     console.log(res.data)
                     // document = render from outside of the script tags. grabbing ('Whatever') by getElementById and making it active by setting up a  .innerHTML to the end of it --> 
                     // then use a Temp Literal// 
                     document.getElementById('weather').innerHTML = `
-                    <h1>City Name: ${res.data.name}</h1>
-                    <h5>Temperature: ${res.data.main.temp}</h5>
-                    <h5>Humidity: ${res.data.main.humidity}</h5>
-                    <h5>Wind Speed: ${res.data.wind.speed}</h5>
-                    <h5>UV Index: ${''}</h5>
-
+                    <div class="dayCard">
+                      <h5>City Name: ${res.data.name}</h5>
+                      <h5>Temperature: ${res.data.main.temp}</h5>
+                      <h5>Humidity: ${res.data.main.humidity}</h5>
+                      <h5>Wind Speed: ${res.data.wind.speed}</h5>
+                    </div>
                 `
                     // need to figure out how to get the temps in F-Degree  //
                     // we found the answer in the API key doc. we had to add " &units=imperial " // 
+
+        getUVIndex(res.data.coord.lat, res.data.coord.lon)
+
                 })
 
                 .catch(err => { console.log(err) })
@@ -46,10 +66,12 @@
                         let forecasterElem = document.createElement('div')
                         // set the innerHTML so that it shows on the page //
                         forecasterElem.innerHTML = `
-                        <h3>City Name: ${forecaster[i].dt_txt}</h3>
-                        <h5>Temperature: ${forecaster[i].main.temp}</h5>
-                        <h5>Humidity: ${forecaster[i].main.humidity}</h5>
-                        <h5>Wind Speed: ${forecaster[i].wind.speed}</h5>           
+                        <div class="cardCaster">
+                          <h3>City Name: ${forecaster[i].dt_txt}</h3>
+                          <h5>Temperature: ${forecaster[i].main.temp}</h5>
+                          <h5>Humidity: ${forecaster[i].main.humidity}</h5>
+                          <h5>Wind Speed: ${forecaster[i].wind.speed}</h5>     
+                        </div>      
                         `
                         // after we MAKE IT we need to grab the forecaster div --> then append it to the bottom of page //
                         document.getElementById('forecaster').append(forecasterElem)
@@ -59,25 +81,46 @@
                 // Ends 2nd axios.get    //
         })
 
-      
-        let uvIndex = document.getElementById('uvIndex').value
-      
-        axios.get(`http://api.openweathermap.org/data/2.5/uvi?appid=606e8adf3bbd843811975dde31618837&lat={lat}&lon={lon}`)
-            .then(res => {
+    //   we are padding lat, lon as parameters //
+        function getUVIndex(lat, lon) {
+            $.ajax({
+              type: "GET",
+              url: "http://api.openweathermap.org/data/2.5/uvi?appid=606e8adf3bbd843811975dde31618837&lat=" + lat + "&lon=" + lon,
+              dataType: "json",
+              success: function(data) {
+                  console.log(data)
+                var uv = $("#uvIndex").text("UV Index: ");
+                var btn = $("<span>").addClass("btn btn-sm").text(data.value);
+                // change color depending on uv value
+                if (data.value < 3) {
+                  btn.addClass("btn-success");
+                }
+                else if (data.value < 7) {
+                  btn.addClass("btn-warning");
+                }
+                else {
+                  btn.addClass("btn-danger");
+                }
+                $("#uvIndex").append(uv.append(btn));
+              }
+            });
+          }
 
-                let uvIndex = res.data.coord
-                for (let i = 5; i < uvIndex.length; i += 8) {
+          var history = JSON.parse(window.localStorage.getItem("history")) || [];
+          for (var i = 0; i < history.length; i++) {
+            makeHistory(history[i]);
+          }
 
-                let uvIndex = document.createElement('div')
-
-                uvIndexElem.innerHTML = `
-                <h5>UV Index: ${uvIndex[i].coord}</h5> 
-                `
-            }
-        })
-            .catch(err => { console.log(err) })
+});
 
 
-            
+
+        //   this is for temp literal and you can import JS values into your HTML
+            //  document.getElementById('x') = `
+            //          ${}
+            //          ${}
+            //          ${}
+            //          ${}
+            // `
 
 
