eecd7cd662df43d192b20de238891be2cbe255d4
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-14 13:12:37 -0800

    temp rendering in F in forecast

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// <!-- **START NOTE** I was working on this in a group with others. I have changed variables and tried to play around with the concept and functions. **END NOTE**  -->
+
 $(document).ready(function () {
 
 
@@ -7,19 +7,15 @@ $(document).ready(function () {
     $(".history").append(li)
   }
 
-  // this is was added to give BUTTON line 17 --> a clickable event by .getElementById() & then .addEventListener to make that button a clickable button. //
   document.getElementById('search').addEventListener('click', event => {
-    // this is so when you serach for a city... it does not refresh the page once it renders //
     event.preventDefault()
 
     let city = document.getElementById('city').value
 
-    // we always want to add ** https:// to all api's -- ALWAYS. -->
-    // once we determined the .then&.catch functions we moved to the temp literal. Then we were able to make a function let city = the value of city on line 13/14 //
-    // **This is just the day's URL weather** //
+    // One Day - Current Weather
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=606e8adf3bbd843811975dde31618837`)
+                                                                    //  add " &units=imperial " //
       .then(res => {
-
 
         if (history.indexOf(city) === -1) {
           history.push(city);
@@ -27,10 +23,8 @@ $(document).ready(function () {
 
           makeHistory(city);
         }
-
+        // Weather API data Info
         console.log(res.data)
-        // document = render from outside of the script tags. grabbing ('Whatever') by getElementById and making it active by setting up a  .innerHTML to the end of it --> 
-        // then use a Temp Literal// 
         document.getElementById('weather').innerHTML = `
                     <div class="dayCard">
                       <h5>City Name: ${res.data.name}</h5>
@@ -38,16 +32,13 @@ $(document).ready(function () {
                       <h5>Humidity: ${res.data.main.humidity}</h5>
                       <h5>Wind Speed: ${res.data.wind.speed}</h5>
                     </div>
-                `
-        // need to figure out how to get the temps in F-Degree  //
-        // we found the answer in the API key doc. we had to add " &units=imperial " // 
-
+                ` 
         getUVIndex(res.data.coord.lat, res.data.coord.lon)
 
       })
       .catch(err => { console.log(err) })
 
-    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=606e8adf3bbd843811975dde31618837`)
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=606e8adf3bbd843811975dde31618837`)
       .then(res => {
         let forecaster = res.data.list
         for (let i = 5; i < forecaster.length; i += 8) {
